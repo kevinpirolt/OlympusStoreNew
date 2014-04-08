fb8a839b10c96afe11dfec94ac7e7abb14d0b04c
thomaspucher <thomaspucher2@gmail.com>
2014-04-08 11:53:28 +0200

    apotheosis done

---
+++ OlympusStoreNew/src/pkgPucher/signin.java
@@ -0,0 +1,113 @@
+package pkgPucher;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+
+import pkgData.Database;
+
+@ManagedBean
+@SessionScoped
+/**
+ * Bean for contentLeft.xhtml (in template folder)
+ */
+public class signin implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@ManagedProperty(value="#{database}")
+	private Database database = null;
+	
+	private String name = "";
+	private String address = "";
+	private String picture = "";
+	private String birthdate = "";
+	private String email = "";
+	private String password = "";
+
+	public signin() {
+	}
+
+	//Getter & Setter
+	
+	public Database getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(Database database) {
+		this.database = database;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	//Getter & Setter
+
+	
+	
+	public String createUser() {
+		
+		try {
+			
+			this.database.createUser(this.getName(), this.getAddress(), this.getPicture(), this.getBirthdate(), this.getEmail(), this.getPassword());
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "signin.jsf";
+		
+	}
+
+	
+}
